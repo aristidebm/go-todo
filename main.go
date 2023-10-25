306d35776ec9929fe04a0b4c444daacbc3f8279a
Aristide <aristide.bamazi@imsp-uac.org>
2023-10-25 23:24:53 +0100

    render todo list

--- main.go
+++ main.go
@@ -12,6 +12,12 @@ type Template struct {
    templates *template.Template 
 }
 
+type Todo struct {
+    Title string
+    Content string
+    Priority string
+}
+
 // implement echo.Renderer interface 
 func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
    return t.templates.ExecuteTemplate(w, name, data)
@@ -24,11 +30,12 @@ func main() {
     }
     router.Renderer = &renderer
     router.GET("/todos", func(c echo.Context) error {
-        name := c.QueryParam("name")
-        if name == "" {
-            name = "Mark"
+        todos := []Todo {
+            {Title: "Touch Typing", Content: "My Learning touch typing with calmack-dh keybord", Priority: "High"},
+            {Title: "English", Content: "I need to learn english", Priority: "Medium"},
+            {Title: "French", Content: "I fluently speak french", Priority: "Low"},
         }
-        return c.Render(http.StatusOK, "todos", name)
+        return c.Render(http.StatusOK, "todos", todos)
     })
     router.Logger.Fatal(router.Start(":8000"))
 }
