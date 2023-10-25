76d83ebc1207effd76adf24af78dfece7358ec96
Aristide <aristide.bamazi@imsp-uac.org>
2023-10-25 22:37:52 +0100

    Render html

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"net/http"
+    "io"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Template struct {
+   templates *template.Template 
+}
+
+// implement echo.Renderer interface 
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+   return t.templates.ExecuteTemplate(w, name, data)
+}
 
 func main() {
-	fmt.Println("Hello World")
+    router := echo.New()
+    renderer := Template{
+        templates: template.Must(template.ParseFiles("todo.html")), 
+    }
+    router.Renderer = &renderer
+    router.GET("/todos", func(c echo.Context) error {
+        name := c.QueryParam("name")
+        if name == "" {
+            name = "Mark"
+        }
+        return c.Render(http.StatusOK, "todos", name)
+    })
+    router.Logger.Fatal(router.Start(":8000"))
 }
