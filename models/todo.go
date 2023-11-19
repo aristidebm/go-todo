29b4aa8b00cd46412275ccc41f664aebde4b4995
Aristide <aristide.bamazi@imsp-uac.org>
2023-11-19 22:58:23 +0100

    Change todo content type from string to sql.NullString

--- models/todo.go
+++ models/todo.go
@@ -1,16 +1,19 @@
 package models
 
-import "fmt"
+import (
+    "fmt"
+    "database/sql"
+)
 
 type Todo struct {
     Id           int       `db:"id"`
     Title        string    `db:"title" form:"title"`
-	Content      string    `db:"content" form:"content"`
+	Content      sql.NullString `db:"content" form:"content"`
     Status       string    `db:"status" form:"status"`
 	Priority     string    `db:"priority" form:"priority"`
 }
 
 func (td *Todo) String() string{
-    return fmt.Sprintf("Todo {Id: %d, Title: %s, Content: %s, Status: %s, Priority: %s}", td.Id, td.Title, td.Content, td.Status, td.Priority) 
+    return fmt.Sprintf("Todo {Title: %s, Status: %s, Priority: %s}",  td.Title, td.Status, td.Priority) 
 }
 
